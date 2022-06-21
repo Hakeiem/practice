4b0fe4af2eace8fa393703fc453bebc89e63220a
Abdulhakim Alsunaydi <a.alsunaydi@tamkeentech.sa>
2022-06-21 13:01:24 +0300

    test

--- practice/resources/views/post/deletePost.blade.php
+++ practice/resources/views/post/deletePost.blade.php
@@ -7,17 +7,18 @@
             'method' => 'DELETE',
             'route' => ['posts.delete', $post->id]
         ]) !!}
-        
+
         <div>
             <h1>{{$post->name}}</h1>
         </div>
         <hr>
+        hfkudody
         <div>
             <h2>{{$post->body}}</h2>
         </div>
         <br>
         {!! Form::submit('Delete Post') !!}
-        
+
         {!! Form::close() !!}
     </body>
 </html>
